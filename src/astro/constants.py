f0835660668820a11946a8c1ab3e06426a538b4a
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2026-09-19 22:46:27 +0800

    Worked out unit conversions

--- src/astro/constants.py
+++ src/astro/constants.py
@@ -1,3 +1,4 @@
+import jax.numpy as jnp
 h = 6.62607015e-34  # Planck constant in J*s
 c = 2.99792458e8    # Speed of light in m/s
 k_B = 1.380649e-23  # Boltzmann constant in J
@@ -6,4 +7,6 @@ sigma = 5.670374419e-8  # Stefan-Boltzmann constant in W/m^2/K^4
 log_nm = jnp.log(1e-9)  # Convert from nanometers to meters
 log_solar_radius = jnp.log(6.957e8)  # Solar radius in meters
 log_solar_luminosity = jnp.log(3.828)+26*jnp.log(10)  # Solar luminosity in Watts
-
+log_pc = jnp.log(3.0856775814913673) + 16 * jnp.log(10)  # Parsec in meters
+log_kpc = log_pc + 3 * jnp.log(10)  # Kiloparsec in meters
+log_jy = 26*jnp.log(10)  # Jansky in W/m^2/Hz
--- src/astro/star.py
+++ src/astro/star.py
@@ -1,23 +1,24 @@
+import jax.numpy as jnp
 from jax import jit, vmap
 from jaxtyping import Array, Float, Int, jaxtyped
 from beartype import beartype
 from typing import TypeVar
 import jax.numpy as jnp
-from constants import h, c, k_B, sigma, log_nm
+from astro.constants import *
 
 @jaxtyped(typechecker=beartype)
 def _B_nu( T: Float[Array, ""], nu: Float[Array, "N"] ) -> Float[Array, "N"]:
     ''' Planck function B_nu(T, nu) for a given frequency nu and temperature T. Units: [W/m^2/Hz/sr] -> [L_sun / R_sun^2 / Hz / sr] '''
     log_numerator = jnp.log(2) + jnp.log(h) - 2 * jnp.log(c) + 3 * jnp.log(nu) # Prevent numerical overflow
     log_denominator = jnp.log(jnp.exp((h / k_B) * (nu / T)) - 1.)
-    return jnp.exp(log_numerator - log_denominator 
+    return jnp.exp(log_numerator - log_denominator - log_solar_luminosity + 2 * log_solar_radius)
 @jaxtyped(typechecker=beartype)
 def _B_lambda( T: Float[Array, ""], lam: Float[Array, "N"]) -> Float[Array, "N"]:
-    ''' Planck function B_lambda(T, lam) for a given wavelength lam and temperature T. Units: [L_sun / R_sun^2 / nm / sr] '''
+    ''' Planck function B_lambda(T, lam) for a given wavelength lam and temperature T. Units: [W/m^2/m/sr] -> [L_sun / R_sun^2 / nm / sr] '''
     lam_meters = lam * 1e-9  # Convert wavelength from nm to meters
     log_numerator = jnp.log(2) + jnp.log(h) + 2 * jnp.log(c) - 5 * jnp.log(lam_meters) # Prevent numerical overflow
     log_denominator = jnp.log(jnp.exp((h * c) / (lam_meters * k_B * T)) - 1.)
-    return jnp.exp(log_numerator - log_denominator + log_W_to_solar_luminosity + 2 * log_meter_to_solar_radius + log_meter_to_nm)
+    return jnp.exp(log_numerator - log_denominator - log_solar_luminosity + 2 * log_solar_radius + log_nm)
 
 # Vectorize all of the above functions using vmap
 def B_nu(
@@ -87,8 +88,8 @@ def L_lambda(
     Returns:
         Spectral luminosity values for the given wavelengths, temperatures, and radii. Units: [Lsun / m]
     """
-    vmap_L_lambda = vmap(_L_lambda, in_axes=(0, 0, None), out_axes=0)
-    return vmap_L_lambda(T, R, lam)
+    return 4 * jnp.pi * R**2 * B_lambda(T, lam)
+
 def L_bolometric(
         T: Float[Array, "M"],
         R: Float[Array, "M"]
@@ -98,13 +99,14 @@ def L_bolometric(
 
     Args:
     T : Temperature in Kelvin.
-    R : Radius in meters.
+    R : Radius in solar radii
 
     Returns:
-        Bolometric luminosity values for the given temperatures and radii. Units: [W]
+        Bolometric luminosity values for the given temperatures and radii. Units: [W] -> [Lsun]
     """
-    vmap_L_bolometric = vmap(_L_bolometric, in_axes=(0, 0), out_axes=0)
-    return vmap_L_bolometric(T, R)
+    R_meters = R * solar_radius  # Convert radius from solar radii to meters
+    log_L_bolometric = jnp.log(4 * jnp.pi) + 2 * jnp.log(R) + jnp.log(sigma) + 4 * jnp.log(T) # Units: W
+    return jnp.exp(log_L_bolometric - log_solar_luminosity)  # Convert to solar luminosities
 def F_bolometric(
         T: Float[Array, "M"],
         R: Float[Array, "M"],
@@ -115,23 +117,24 @@ def F_bolometric(
 
     Args:
     T : Temperature in Kelvin.
-    R : Radius in meters.
-    d : Distance in *pc*
+    R : Radius in solar radii
+    d : Distance in *kpc*
 
     Returns:
-        Bolometric flux values for the given temperatures, radii, and distances. Units: [W/m^2]
+        Bolometric flux values for the given temperatures, radii, and distances. Units: [Lsun/kpc] -> [Jy*Hz]
     """
     L_bol = L_bolometric(T, R)
-    return L_bol / (4 * jnp.pi * d**2)
-
-
+    log_F_bolometric = jnp.log(L_bol) - jnp.log(4 * jnp.pi) - 2 * jnp.log(d) # Units: [Lsun/kpc]
+    # Convert Lsun/kpc^2 to W/m^2 and then to Jy*Hz
+    log_F_bolometric = log_F_bolometric + log_solar_luminosity - 2 * log_kpc - log_jansky
+    return jnp.exp(log_F_bolometric)
 
 if __name__ == "__main__":
     # Example usage
-    T = jnp.array([3e3, 4e3, 5e3])
-    R = jnp.array([1e9, 2e9, 3e9])  # Radii in meters
+    T = jnp.array([3e3, 4e3, 5e3]) # Temperatures in Kelvin
+    R = jnp.array([1.0, 2.0, 3.0]) # Radii in solar radii
     nu = jnp.array([1e14, 2e14, 3e14])  # Frequencies in Hz
-    lam = jnp.array([1e-6, 2e-6, 3e-6])  # Wavelengths in meters
+    lam = jnp.array([400., 500., 600.])  # Wavelengths in nm
     print("B_nu:", B_nu(T, nu))
     print("B_lambda:", B_lambda(T, lam)) 
     print("L_nu:", L_nu(T, R, nu))
