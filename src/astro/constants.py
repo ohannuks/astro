ac93c20640208ae6c94e3c4582d7d0964188258c
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2026-09-19 22:20:35 +0800

    update

--- src/astro/constants.py
+++ src/astro/constants.py
@@ -2,4 +2,8 @@ h = 6.62607015e-34  # Planck constant in J*s
 c = 2.99792458e8    # Speed of light in m/s
 k_B = 1.380649e-23  # Boltzmann constant in J
 sigma = 5.670374419e-8  # Stefan-Boltzmann constant in W/m^2/K^4
+# Unit conversions
+log_nm = jnp.log(1e-9)  # Convert from nanometers to meters
+log_solar_radius = jnp.log(6.957e8)  # Solar radius in meters
+log_solar_luminosity = jnp.log(3.828)+26*jnp.log(10)  # Solar luminosity in Watts
 
--- src/astro/star.py
+++ src/astro/star.py
@@ -3,33 +3,21 @@ from jaxtyping import Array, Float, Int, jaxtyped
 from beartype import beartype
 from typing import TypeVar
 import jax.numpy as jnp
-from constants import h, c, k_B, sigma
+from constants import h, c, k_B, sigma, log_nm
 
 @jaxtyped(typechecker=beartype)
 def _B_nu( T: Float[Array, ""], nu: Float[Array, "N"] ) -> Float[Array, "N"]:
+    ''' Planck function B_nu(T, nu) for a given frequency nu and temperature T. Units: [W/m^2/Hz/sr] -> [L_sun / R_sun^2 / Hz / sr] '''
     log_numerator = jnp.log(2) + jnp.log(h) - 2 * jnp.log(c) + 3 * jnp.log(nu) # Prevent numerical overflow
     log_denominator = jnp.log(jnp.exp((h / k_B) * (nu / T)) - 1.)
-    return jnp.exp(log_numerator - log_denominator)
-
+    return jnp.exp(log_numerator - log_denominator 
 @jaxtyped(typechecker=beartype)
-def _B_lambda(
-        T: Float[Array, ""],
-        lam: Float[Array, "N"]
-        ) -> Float[Array, "N"]:
-    log_numerator = jnp.log(2) + jnp.log(h) + 2 * jnp.log(c) - 5 * jnp.log(lam) # Prevent numerical overflow
-    log_denominator = jnp.log(jnp.exp((h * c) / (lam * k_B * T)) - 1.)
-    return jnp.exp(log_numerator - log_denominator)
-
-# Convert to spectral luminosity:
-@jaxtyped(typechecker=beartype)
-def _L_nu( T: Float[Array, ""], R: Float[Array, ""], nu: Float[Array, "N"]) -> Float[Array, "N"]:
-    return 4 * jnp.pi * R**2 * B_nu(nu, T)
-
-def _L_lambda( T: Float[Array, ""], R: Float[Array, ""], lam: Float[Array, "N"],) -> Float[Array, "N"]:
-    return 4 * jnp.pi * R**2 * B_lambda(lam, T)
-# Stefann Boltzmann law: L = 4 * pi * R^2 * sigma * T^4
-def _L_bolometric(T: Float[Array, ""], R: Float[Array, ""]) -> Float[Array, ""]:
-    return 4 * jnp.pi * R**2 * sigma * T**4
+def _B_lambda( T: Float[Array, ""], lam: Float[Array, "N"]) -> Float[Array, "N"]:
+    ''' Planck function B_lambda(T, lam) for a given wavelength lam and temperature T. Units: [L_sun / R_sun^2 / nm / sr] '''
+    lam_meters = lam * 1e-9  # Convert wavelength from nm to meters
+    log_numerator = jnp.log(2) + jnp.log(h) + 2 * jnp.log(c) - 5 * jnp.log(lam_meters) # Prevent numerical overflow
+    log_denominator = jnp.log(jnp.exp((h * c) / (lam_meters * k_B * T)) - 1.)
+    return jnp.exp(log_numerator - log_denominator + log_W_to_solar_luminosity + 2 * log_meter_to_solar_radius + log_meter_to_nm)
 
 # Vectorize all of the above functions using vmap
 def B_nu(
@@ -37,31 +25,30 @@ def B_nu(
         nu: Float[Array, "N"]
         ) -> Float[Array, "M N"]:
     """
-    Planck function B_nu(nu, T) for a given frequency nu and temperature T.
+    Planck function B_nu(T, nu) for a given frequency nu and temperature T.
 
     Args:
     nu: Frequency in Hz.
     T : Temperature in Kelvin.
 
     Returns:
-        Planck function values for the given frequencies and temperatures. Units: [W/m^2/Hz]
+        Planck function values for the given frequencies and temperatures. Units: [L_sun / R_sun^2 / Hz / sr]
     """
     vmap_B_nu = vmap(_B_nu, in_axes=(0, None), out_axes=0)
     return vmap_B_nu(T, nu)
-
 def B_lambda(
         T: Float[Array, "M"],
         lam: Float[Array, "N"]
         ) -> Float[Array, "M N"]:
     """
-    Planck function B_lambda(lam, T) for a given wavelength lam and temperature T.
+    Planck function B_lambda(T, lam) for a given wavelength lam and temperature T.
 
     Args:
-    lam: Wavelength in meters.
+    lam: Wavelength in nanometers
     T : Temperature in Kelvin.
 
     Returns:
-        Planck function values for the given wavelengths and temperatures. Units: [W/m^2/m]
+        Planck function values for the given wavelengths and temperatures. Units: [L_sun / R_sun^2 / nm / sr]
     """
     vmap_B_lambda = vmap(_B_lambda, in_axes=(0, None), out_axes=0)
     return vmap_B_lambda(T, lam)
@@ -72,18 +59,17 @@ def L_nu(
         nu: Float[Array, "N"]
         ) -> Float[Array, "M N"]:
     """
-    Spectral luminosity L_nu(nu, T) for a given frequency nu, temperature T, and radius R.
+    Spectral luminosity L_nu(T, nu) for a given frequency nu, temperature T, and radius R.
 
     Args:
     nu: Frequency in Hz.
     T : Temperature in Kelvin.
-    R : Radius in meters.
+    R : Radius in solar radii
 
     Returns:
-        Spectral luminosity values for the given frequencies, temperatures, and radii. Units: [W/Hz]
+        Spectral luminosity values for the given frequencies, temperatures, and radii. Units: [Lsun / Hz]
     """
-    vmap_L_nu = vmap(_L_nu, in_axes=(0, 0, None), out_axes=0)
-    return vmap_L_nu(T, R, nu)
+    return 4 * jnp.pi * R**2 * B_nu(T, nu)
 
 def L_lambda(
         T: Float[Array, "M"],
@@ -91,15 +77,15 @@ def L_lambda(
         lam: Float[Array, "N"]
         ) -> Float[Array, "M N"]:
     """
-    Spectral luminosity L_lambda(lam, T) for a given wavelength lam, temperature T, and radius R.
+    Spectral luminosity L_lambda(T, lam) for a given wavelength lam, temperature T, and radius R.
 
     Args:
-    lam: Wavelength in meters.
+    lam: Wavelength in nm.
     T : Temperature in Kelvin.
-    R : Radius in meters.
+    R : Radius in solar radii
 
     Returns:
-        Spectral luminosity values for the given wavelengths, temperatures, and radii. Units: [W/m]
+        Spectral luminosity values for the given wavelengths, temperatures, and radii. Units: [Lsun / m]
     """
     vmap_L_lambda = vmap(_L_lambda, in_axes=(0, 0, None), out_axes=0)
     return vmap_L_lambda(T, R, lam)
@@ -119,6 +105,25 @@ def L_bolometric(
     """
     vmap_L_bolometric = vmap(_L_bolometric, in_axes=(0, 0), out_axes=0)
     return vmap_L_bolometric(T, R)
+def F_bolometric(
+        T: Float[Array, "M"],
+        R: Float[Array, "M"],
+        d: Float[Array, "M"]
+        ) -> Float[Array, "M"]:
+    """
+    Bolometric flux F_bolometric(T, R, d) for a given temperature T, radius R, and distance d.
+
+    Args:
+    T : Temperature in Kelvin.
+    R : Radius in meters.
+    d : Distance in *pc*
+
+    Returns:
+        Bolometric flux values for the given temperatures, radii, and distances. Units: [W/m^2]
+    """
+    L_bol = L_bolometric(T, R)
+    return L_bol / (4 * jnp.pi * d**2)
+
 
 
 if __name__ == "__main__":
