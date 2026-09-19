06fe5439a45ca515afb7701366ed6d2f8d2a0f48
Otto Akseli Hannuksela <otto.akseli.hannuksela@gmail.com>
2026-09-19 20:41:14 +0800

    Update comments to google style

---
+++ src/astro/constants.py
@@ -0,0 +1,3 @@
+h = 6.62607015e-34  # Planck constant in J*s
+c = 2.99792458e8    # Speed of light in m/s
+k_B = 1.380649e-23  # Boltzmann constant in J
--- src/astro/star.py
+++ src/astro/star.py
@@ -1,7 +1,9 @@
 from jaxtyping import Array, Float64, Int32, jaxtyped
 from beartype import beartype
 import jax.numpy as jnp
+from constants import h, c, k_B
 
+@jaxtyped(typechecker=beartype)
 def B_nu(
         nu: Float64[Array, "N"], 
         T: Float64[Array, "N"]
@@ -10,13 +12,30 @@ def B_nu(
     Compute the Planck function B_nu(nu, T) for a given frequency nu and temperature T.
 
     Parameters
-    nu : Float64[Array, "N"]
-        Frequency array in Hz.
-    T : Float64[Array, "N"]
-        Temperature array in Kelvin.
+    nu: Frequency array in Hz.
+    T: Temperature array in Kelvin.
 
-    Returns
-    Float64[Array, "N"]:
+    Returns: 
         Planck function values for the given frequencies and temperatures.
     """
+    return (2 * h * nu**3 / c**2) / (jnp.exp(h * nu / (k_B * T)) - 1)
+
+@jaxtyped(typechecker=beartype)
+def B_lambda(
+        lam: Float64[Array, "N"],
+        T: Float64[Array, "N"]
+        ) -> Float64[Array, "N"]:
+    """
+    Compute the Planck function B_lambda(lambda, T) for a given wavelength lambda and temperature T.
+
+    Args:
+    lam: Wavelength array in meters.
+    T : Temperature array in Kelvin.
+
+    Returns:
+        Planck function values for the given wavelengths and temperatures.
+    """
+    return (2 * h * c**2 / lam**5) / (jnp.exp(h * c / (lam * k_B * T)) - 1)
+
+
 
